06eb2df06ab6b44fa4d9f576627cd4250154979a
Michael <Michael.Bardwell.Scott@Gmail.com>
2019-06-07 21:15:35 -0400

    created header & router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CurrentComponent } from './current/current.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CurrentComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/forecast/forecast.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'det-forecast',
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.css']
+})
+export class ForecastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
